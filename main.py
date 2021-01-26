a15446af4d706350cd77606089089f7155d0144c
Hayden Donnelly <Bizbud@live.com>
2021-01-26 17:42:02 -0500

    changed directory structure to accomodate processing

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    #if client.user.mention in message.content.split(" "):
+    if client.user.mentioned_in(message):
+        await message.channel.send("Nyeah eh? Waste yute tryna mess with me eh?? Mans bout to catch this defaz real quick")
     if message.content.startswith(command_prefix + 'say'):
-        await message.delete();
+        await message.delete()
         await message.channel.send(message.content.split("/say",1)[1])
 
 token = input()
