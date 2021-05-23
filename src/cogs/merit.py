9e6cafa797de4851fe5f02b3d6e1a347994e58c2
Hayden Donnelly <Bizbud@live.com>
2021-05-22 22:12:10 -0400

    updates to merit system, but orca is not working which means plot figure can't be saved to images

--- src/cogs/merit.py
+++ src/cogs/merit.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
 import pandas as pd
+import plotly.express as px
 from datetime import date
-import matplotlib.pyplot as plt
-import seaborn as sns
+import global_vars as gv
 
 class Merit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         try:
-            self.merit_log = pd.read_csv('../../test_data.csv')
+            self.merit_log = pd.read_csv(gv.hidden_file_path + 'test_data.csv', index_col='Date', parse_dates=True)
         except:
             self.merit_log = pd.DataFrame()
 
@@ -17,7 +17,7 @@ class Merit(commands.Cog):
         entry = {'Guild ID': ctx.message.guild.id, 'User ID': user_id, 
                 'Merit': 1, 'Demerit': 0, 'Date': str(date.today())}
         self.merit_log = self.merit_log.append(entry, ignore_index=True)
-        self.merit_log.to_csv('../../test_data.csv')
+        self.merit_log.to_csv(gv.hidden_file_path + 'test_data.csv')
 
     @commands.command()
     @commands.has_role("Admin")
@@ -33,15 +33,15 @@ class Merit(commands.Cog):
 
     @commands.command()
     @commands.has_role("Admin")
-    async def merit_history(self, ctx):
-        data = pd.DataFrame(self.merit_log.groupby('Date', as_index=False)['Merit'].count())
-        data = pd.DataFrame({'Date': data['Date'], 'Merit': data['Merit']})
-        plot = sns.lineplot(data=data, x ='Date', y='Merit')
-        plt.xticks(rotation=20)
-        plt.rcParams['savefig.dpi'] = 300
-        plot.get_figure().savefig('../../merit_history.png')
-        plot.get_figure().clf()
-        await ctx.send(file=discord.File('../../merit_history.png'))
+    async def merit_history(self, ctx, user_id):
+        data = self.merit_log[(self.merit_log['Guild ID'] == ctx.message.guild.id) & (self.merit_log['User ID'] == user_id)]
+        data = data.drop(columns=['Guild ID', 'User ID'])
+        data = data.resample('D').sum()
+        data = data.reset_index()
+
+        fig = px.line(x='Date', y=['Merit', 'Demerit'], data_frame=data, title='Merit/Demerit History')
+        fig.write_image(gv.hidden_file_path + 'merit_history.png')
+        await ctx.send(file=discord.File(gv.hidden_file_path + 'merit_history.png'))
 
 def setup(bot):
     bot.add_cog(Merit(bot))
--- src/global_vars.py
+++ src/global_vars.py
@@ -5,18 +5,19 @@ import os
 import time
 
 def init():
-    global prefix, bot, help_text_file, help_text, keys, discord_key, mw_collegiate_key, mw_medical_key, settings, start_time
+    global prefix, bot, hidden_file_path, help_text_file, help_text, keys, discord_key, mw_collegiate_key, mw_medical_key, settings, start_time
     prefix = "?"
     bot = commands.Bot(command_prefix=prefix)
-    help_text_file = open(os.path.abspath("data/help.txt"))
+    hidden_file_path = '../../'
+    help_text_file = open(os.path.abspath('data/help.txt'))
     help_text = help_text_file.read()
 
-    keys = pd.read_csv(os.path.abspath("../../api_keys.csv"))
+    keys = pd.read_csv(os.path.abspath(hidden_file_path + 'api_keys.csv'))
     discord_key = str(keys[keys['API'] == 'Discord']['Key'].values[0])
     mw_collegiate_key = str(keys[keys['API'] == 'MW Collegiate']['Key'].values[0])
     mw_medical_key = str(keys[keys['API'] == 'MW Medical']['Key'].values[0])
 
-    settings = pd.read_csv(os.path.abspath("../../settings.csv"))
+    settings = pd.read_csv(os.path.abspath(hidden_file_path + 'settings.csv'))
     settings = settings.set_index('Guild ID')
 
     start_time = time.time()
